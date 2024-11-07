ac45792091c3498a03853e309b70ee15dad1ccec
Xeno <131300185+Diego-Ulpin12@users.noreply.github.com>
2024-11-06 21:46:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Steam</title>
+    <title>Formulario</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,44 +10,43 @@
     <link rel="stylesheet" href="css/bootstrap.css">
   </head>
   <body>
-      <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <form action="procesar.php" method="POST" enctype="multipart/form-data">
-                    <div class="mb-3">
-                        <label for="nombre" class="form-label">Nombre Completo</label>
-                        <input type="text" class="form-control" id="nombre" name="nombre">
-                    </div>
+    <?php require_once "header.php" ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <form action="procesar.php" method="POST" enctype="multipart/form-data">
+                        <form action="codigo_qr,php" method="post" enctype="multipart/form.data"></form>
+                            <div class="mb-3">
+                                <label for="nombre" class="form-label">Nombre Completo</label>
+                                <input type="text" class="form-control" id="nombre" name="nombre">
+                            </div>
 
-                    <div class="mb-3">
-                        <label for="rut" class="form-label">RUT</label>
-                        <input type="text" class="form-control" id="rut" name="rut">
-                    </div>
+                            <div class="mb-3">
+                                <label for="rut" class="form-label">RUT</label>
+                                <input type="text" class="form-control" id="rut" name="rut">
+                            </div>
 
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="text" class="form-control" id="email" name="email">
-                    </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="text" class="form-control" id="email" name="email">
+                            </div>
 
-                    <div class="mb-3">
-                        <label for="telefono" class="form-label">Telefono</label>
-                        <input type="text" class="form-control" id="telefono" name="telefono">
-                    </div>
+                            <div class="mb-3">
+                                <label for="telefono" class="form-label">Telefono</label>
+                                <input type="text" class="form-control" id="telefono" name="telefono">
+                            </div>
 
-                    <div class="mb-3">
-                        <label for="imagen" class="form-label">Imagen asistente</label>
-                        <input class="form-control" type="file" id="imagen" name="imagen">
-                    </div>
+                            <div class="mb-3">
+                                <label for="imagen" class="form-label">Imagen asistente</label>
+                                <input class="form-control" type="file" id="imagen" name="imagen">
+                            </div>
 
-                    <?php
-                        include "phpqrcode/qrlib.php";
-                        QRcode::png('code data text', 'filename.png'); // creates file
-                    ?>
-
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
+        <?php require_once "footer.php"; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
